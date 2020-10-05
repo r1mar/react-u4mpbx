e3847de40a7f689bcbf27526d63f4c9e2d0e16a3
richard-martens <richard.martens@outlook.com>
2020-10-05 22:03:27 +0200

    Stattgaben verlinkt, aber nicht angelegt

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -6,7 +6,7 @@ export default function StartMenu() {
   return (
     <ul>
       <li>
-        <Link to="start-game">Neues Spiel</Link>
+        <Link to="/start-game">Neues Spiel</Link>
       </li>
     </ul>
   );
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import StartMenu from "./StartMenu";
 import NotFound from "./NotFound";
+import StartGane from "./StartGame";
 
 ReactDOM.render(
   <Router>
       <Switch>
         <Route exact path="/" component={StartMenu} />
+        <Route path="/start-game" component={StartGane} />
         <Route component={NotFound} />
       </Switch>
   </Router>, document.getElementById("root"));
