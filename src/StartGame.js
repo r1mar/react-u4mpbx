809f1e5ff61411ebcc3adaa2d1eed008c27cc76f
richard-martens <richard.martens@outlook.com>
2020-10-11 08:05:05 +0200

    3 files modified

--- src/StartGame.js
+++ src/StartGame.js
@@ -1,14 +1,35 @@
 import React from "react";
 import style from "./style.css";
-import RoundQueue from "./RoundQueue";
 
-export default function StartGame() {
+export default class StartGame 
+extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: {
+        class: "ship",
+        position: {
+          x: "10em",
+          y: "10em"
+        }
+      }
+    }
+  }
+
+  render() {
   return (
     <div className="gameArea">
-      <RoundQueue />
+      <div className="ship" style={
+        {
+          top: this.state.active.position.y,
+          left: this.state.active.position.x
+        }
+      } />
       <div className="orbit">
         <div className="planet" />
       </div>
     </div>
   );
+  }
 }
