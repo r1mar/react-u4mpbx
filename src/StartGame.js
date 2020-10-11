7d34dea0c5c035c7b4208019b5ca38b65f3b920a
richard-martens <richard.martens@outlook.com>
2020-10-11 22:18:18 +0200

    1 file modified

--- src/StartGame.js
+++ src/StartGame.js
@@ -7,12 +7,14 @@ extends React.Component {
     super(props);
 
     this.state = {
+      area: {
+        width: 100,
+        height: 100
+      },
       active: {
         class: "ship blink",
-        position: {
-          x: "10em",
-          y: "10em"
-        }
+        x: 50,
+        y: 50
       }
     };
 
