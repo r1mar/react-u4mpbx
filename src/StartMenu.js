5d63383e36a5a1b9f6291f6da6e247093344124d
richard-martens <richard.martens@outlook.com>
2020-10-26 21:01:00 +0100

    1 file modified

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -10,6 +10,13 @@ export default class StartMenu extends React.Component {
   }
 
   download() {
+    let data = "data:text/json;charset=utf8," + encodeURIComponent(JSON.stringify({test: "test"})),
+       aDownload = document.getElementById("downloadDummy");
+
+    aDownload.setAttribute("href", data);
+    aDownload.setAttribute("download", "data.json");
+
+    aDownload.click();
 
   }
 
@@ -19,6 +26,7 @@ export default class StartMenu extends React.Component {
       <li>
         <Link className="nav-link" to="/teams">Vereine</Link>
         <button className="nav-link" onClick={this.download}>Speichern</button>
+        <a id="downloadDummy" style={{display: "none" }} />
       </li>
     </ul>
   );
