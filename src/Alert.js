373f42f7baf6f98cfa205b65dfeffda2988fb5b3
richard-martens <richard.martens@outlook.com>
2020-11-16 06:22:59 +0100

    1 file modified

--- src/Alert.js
+++ src/Alert.js
@@ -4,9 +4,9 @@ import FieldError from "./FieldError";
 export default function Alert(props) {
   let messages = props.messages
     .filter(message => !(message instanceof FieldError))
-    .map(message => {
-      <div className="alert alert-danger">{message.message}</div>;
-    });
+    .map(message => (
+      <div className="alert alert-danger">{message.message}</div>
+    ));
 
-  return <div>{messages} {props.messages.length ? props.messages[0].message : JSON.stringify(props.messages)}</div>;
+  return <div>{messages}</div>;
 }
