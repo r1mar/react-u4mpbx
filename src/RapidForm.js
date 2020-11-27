80ec5e8cf1d050d7dedffb99c0cb9b0c9e89eedf
richard-martens <richard.martens@outlook.com>
2020-11-27 13:59:34 +0100

    1 file modified

--- src/RapidForm.js
+++ src/RapidForm.js
@@ -1,13 +1,46 @@
 import React from "react";
-import Alert from "./Alert";
+import Form from "./Form";
 
-export default function Form(props) {
-  return (
-    <form onSubmit={props.onSubmit} className={props.validated ? "form-validated" : ""}>
-      {props.children}
+export default class RapidForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <input type="submit" className="btn btn-primary" />
-      <Alert messages={props.errors} />
-    </form>
-  );
+    this.state = {
+      object: {},
+      metadata: {},
+      errors: []
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  async componentDidMount() {
+    try {
+      this.setState({
+        metadata: await service.readMetadata(this.props.meta)
+      });
+    } catch (e) {
+      this.setState({
+        errors: [e]
+      });
+    }
+  }
+
+  onSubmit() {
+
+  }
+
+  render() {
+    return (
+      <Form
+        onSubmit={this.onSubmit}
+        className={props.validated ? "form-validated" : ""}
+      >
+        {props.children}
+
+        <input type="submit" className="btn btn-primary" />
+        <Alert messages={props.errors} />
+      </Form>
+    );
+  }
 }
